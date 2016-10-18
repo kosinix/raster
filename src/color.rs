097e49a8d343cf96ba1e75951b66133c5c982165
kosinix <kosinix@users.noreply.github.com>
2016-10-18 10:45:56 +0800

    Add color struct

---
+++ src/color.rs
@@ -0,0 +1,26 @@
+//!  Image module for handling colors.
+
+// crates
+
+// from rust
+
+// from external crate
+
+// from local crate
+
+/// A struct for representing an RGBA color.
+#[derive(Debug)]
+pub struct Color {
+    /// Red channel 0 - 255
+    pub r: u8,
+    
+    /// Green channel 0 - 255
+    pub g: u8,
+
+    /// Blue channel 0 - 255
+    pub b: u8,
+
+    /// Alpha channel 0 - 255
+    pub a: u8,
+}
+
