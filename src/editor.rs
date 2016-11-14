ebb167f586165914bfe37cfc92bec44da8c49667
kosinix <kosinix@users.noreply.github.com>
2016-11-14 14:32:15 +0800

    Use 2 pass bilinear interpolation

--- src/editor.rs
+++ src/editor.rs
@@ -462,64 +462,113 @@ fn interpolate_nearest(src: &Image, w: i32, h: i32) -> Result<Image, String> {
 // interpolate using linear function
 fn interpolate_bilinear(src: &Image, w2: i32, h2: i32) -> Result<Image, String> {
     
+    let mut dest = try!(interpolate_bilinear_width(&src, w2));
+    dest = try!(interpolate_bilinear_height(&dest, h2));
+    
+    Ok(dest)
+}
+
+fn interpolate_bilinear_width(src: &Image, w2: i32) -> Result<Image, String> {
+    
     let w1 = src.width;
     let h1 = src.height;
 
     let x_ratio: f64 = w1 as f64 / w2 as f64;
-    let y_ratio: f64 = h1 as f64 / h2 as f64;
     
-    let mut dest = Image::blank(w2, h2);
+    let mut dest = Image::blank(w2, h1);
     
     let offset_x = (w2 / w1 / 2) as i32;
-    let offset_y = (h2 / h1 / 2) as i32;
 
-    for y in 0..h2 {
-        for x in 0..w2 {
+    let x_start = 0 - offset_x;
+    let x_end = w2 - offset_x;
+
+    for y in 0..h1 {
+        for x in x_start..x_end {
             
-            let src_x = x as f64 * x_ratio;
-            let src_y = y as f64 * y_ratio;
+            let mut src_x = x as f64 * x_ratio;
+            if src_x < 0.0 {
+                src_x = 0.0; // limit lower bound to 0
+            }
 
             let src_x_int = (src_x).floor() as i32;
-            let src_y_int = (src_y).floor() as i32;
 
             let src_x_int2 = cmp::min(src_x_int + 1, w1-1); // limit range withn $w1-1
-            let src_y_int2 = cmp::min(src_y_int + 1, h1-1); // limit range withn $h1-1
 
             // limit range from 0 - 1
             let t_x = src_x - src_x_int as f64;
-            let t_y = src_y - src_y_int as f64;
 
-            let src_color1 = try!(src.get_pixel(src_x_int, src_y_int));
-            let src_color2 = try!(src.get_pixel(src_x_int2, src_y_int));
-            let src_color3 = try!(src.get_pixel(src_x_int, src_y_int2));
-            let src_color4 = try!(src.get_pixel(src_x_int2, src_y_int2));
-
+            let src_color1 = try!(src.get_pixel(src_x_int, y));
+            let src_color2 = try!(src.get_pixel(src_x_int2, y));
+            
             // red
-            let red = _lerp(_lerp(src_color1.r, src_color3.r, t_y), _lerp(src_color2.r, src_color4.r, t_y), t_x);
+            let red = _lerp(src_color1.r, src_color2.r, t_x);
 
             // green
-            let green = _lerp(_lerp(src_color1.g, src_color3.g, t_y), _lerp(src_color2.g, src_color4.g, t_y), t_x);
+            let green = _lerp(src_color1.g, src_color2.g, t_x);
 
             // blue
-            let blue = _lerp(_lerp(src_color1.b, src_color3.b, t_y), _lerp(src_color2.b, src_color4.b, t_y), t_x);
+            let blue = _lerp(src_color1.b, src_color2.b, t_x);
 
-            // alpha. TODO: maybe blend alpha from 4 points?
-            let alpha = src_color1.a;
+            // alpha
+            let alpha = _lerp(src_color1.a, src_color2.a, t_x);
 
-            try!(dest.set_pixel(x, y, Color::rgba(red, green, blue, alpha)));
+            try!(dest.set_pixel(x+offset_x, y, Color::rgba(red, green, blue, alpha)));
 
         }
     }
-    // $image3 = imagecreatetruecolor($w2, $h2);
-    // imagecopy($image3, $image2, $offset_x, $offset_y, 0, 0, $w2-$offset_x, $h2-$offset_y);
+    
+    Ok(dest)
+}
 
-    // for($x = 0; $x < $offset_x; $x++){
-    //     imagecopy($image3, $image3, $x, $offset_y, $offset_x, $offset_y, 1, $h2-$offset_y);
-    // }
+fn interpolate_bilinear_height(src: &Image, h2: i32) -> Result<Image, String> {
+    
+    let w1 = src.width;
+    let h1 = src.height;
 
-    // for($y = 0; $y < $offset_y; $y++){
-    //     imagecopy($image3, $image3, 0, $y, 0, $offset_y, $w2, 1);
-    // }
+    let y_ratio: f64 = h1 as f64 / h2 as f64;
+    
+    let mut dest = Image::blank(w1, h2);
+    
+    let offset_y = (h2 / h1 / 2) as i32;
+
+    let y_start = 0 - offset_y;
+    let y_end = h2 - offset_y;
+
+    for x in 0..w1 {
+        for y in y_start..y_end {
+            
+            let mut src_y = y as f64 * y_ratio;
+
+            if src_y < 0.0 {
+                src_y = 0.0; // limit lower bound to 0
+            }
+
+            let src_y_int = (src_y).floor() as i32;
+
+            let src_y_int2 = cmp::min(src_y_int + 1, h1-1); // limit range withn $h1-1
+
+            // limit range from 0 - 1
+            let t_y = src_y - src_y_int as f64;
+
+            let src_color1 = try!(src.get_pixel(x, src_y_int));
+            let src_color2 = try!(src.get_pixel(x, src_y_int2));
+            
+            // red
+            let red = _lerp(src_color1.r, src_color2.r, t_y);
+
+            // green
+            let green = _lerp(src_color1.g, src_color2.g, t_y);
+
+            // blue
+            let blue = _lerp(src_color1.b, src_color2.b, t_y);
+
+            // alpha
+            let alpha = _lerp(src_color1.a, src_color2.a, t_y);
+
+            try!(dest.set_pixel(x, y+offset_y, Color::rgba(red, green, blue, alpha)));
+
+        }
+    }
     
     Ok(dest)
 }
