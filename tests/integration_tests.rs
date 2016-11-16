173eb16385d407c55d9dd6ca283c5a2f1df05d44
kosinix <kosinix@users.noreply.github.com>
2016-11-16 11:25:21 +0800

    Tidy integration_tests and add prepare step before running tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,8 +1,8 @@
 extern crate image;
 
 #[test]
-fn check_open() {
-    // let res = image::open("nonexistent");
-    // println!("{:?}", res);
-    assert_eq!(true, true);
+fn prepare_test() {
+    let _ = std::fs::create_dir_all("./tests/out"); // Make sure test out dir is present. This might be a symptom that we need better error handling in editor::save
 }
+
+// TODO: test open and save
