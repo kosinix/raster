8269bb8f2c405b2aa35c17555dd2ca13f24ea0f7
kosinix <kosinix@users.noreply.github.com>
2016-12-18 20:55:51 +0800

    Update filter test factor

--- tests/filter_tests.rs
+++ tests/filter_tests.rs
@@ -6,6 +6,6 @@ use raster::filter;
 fn brightness_test(){
 
     let mut image = raster::open("tests/in/sample.jpg").unwrap();
-    filter::brightness(&mut image, 1.2).unwrap();
+    filter::brightness(&mut image, 1.5).unwrap();
     raster::save(&image, "tests/out/test_filter_brightness.jpg");
 }
