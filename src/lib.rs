44a09b9d32ca9bbda504f5bdf848a95f0c31a5a9
kosinix <kosinix@users.noreply.github.com>
2017-01-08 08:19:11 +0800

    Remove commented out code

--- src/lib.rs
+++ src/lib.rs
@@ -869,7 +869,6 @@ fn _encode_png(image: &Image, path: &Path) -> RasterResult<()>{
     let ref mut w = BufWriter::new(file);
 
     let mut encoder = png::Encoder::new(w, image.width as u32, image.height as u32);
-    // encoder.set(png::ColorType::RGBA).set(png::BitDepth::Eight);
     png::HasParameters::set(&mut encoder, png::ColorType::RGBA);
     png::HasParameters::set(&mut encoder, png::BitDepth::Eight);
     let mut writer = try!(encoder.write_header());
