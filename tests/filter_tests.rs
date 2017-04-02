315ea5e05ddfa856cd321c69e236f1964e0fa475
Lukas Abfalterer <labfalterer@gmail.com>
2017-04-02 15:58:37 +0200

    fixes #13 adding a sobel filter to the library you can choose each direction you want

--- tests/filter_tests.rs
+++ tests/filter_tests.rs
@@ -1,6 +1,6 @@
 extern crate raster;
 
-use raster::filter;
+use raster::{filter, Orientation};
 
 #[test]
 fn brightness_test(){
@@ -8,4 +8,39 @@ fn brightness_test(){
     let mut image = raster::open("tests/in/sample.jpg").unwrap();
     filter::brightness(&mut image, 1.5).unwrap();
     raster::save(&image, "tests/out/test_filter_brightness.jpg").unwrap();
+}
+
+#[test]
+fn sobel_test() {
+    let mut image = raster::open("tests/in/sample.jpg").unwrap();
+    filter::sobel(&mut image, Orientation::Both).unwrap();
+    raster::save(&image, "tests/out/test_filter_sobel.jpg").unwrap();
+}
+
+#[test]
+fn sobel_x_test() {
+    let mut image = raster::open("tests/in/sample.jpg").unwrap();
+    filter::sobel(&mut image, Orientation::Horizontal).unwrap();
+    raster::save(&image, "tests/out/test_filter_sobel_x.jpg").unwrap();
+}
+
+#[test]
+fn sobel_y_test() {
+    let mut image = raster::open("tests/in/sample.jpg").unwrap();
+    filter::sobel(&mut image, Orientation::Vertical).unwrap();
+    raster::save(&image, "tests/out/test_filter_sobel_y.jpg").unwrap();
+}
+
+#[test]
+fn sobel_d1_test() {
+    let mut image = raster::open("tests/in/sample.jpg").unwrap();
+    filter::sobel(&mut image, Orientation::DiagonalUp).unwrap();
+    raster::save(&image, "tests/out/test_filter_sobel_d1.jpg").unwrap();
+}
+
+#[test]
+fn sobel_d2_test() {
+    let mut image = raster::open("tests/in/sample.jpg").unwrap();
+    filter::sobel(&mut image, Orientation::DiagonalDown).unwrap();
+    raster::save(&image, "tests/out/test_filter_sobel_d2.jpg").unwrap();
 }
